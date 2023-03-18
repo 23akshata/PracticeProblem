82d9176f7f7a7dc32174ab90e224710e4f026465
akshata <akshata2018p@gamil.com>
2023-03-18 12:30:53 +0530

    [akshata]Add:Create Object Class

---
+++ PracticeProgram/ObjectClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProgram
+{
+    public class ObjectClass
+    {
+        public int id;
+        public string name;
+        public static void Student()
+        {
+            ObjectClass objectClass = new ObjectClass();
+            objectClass.id = 10;
+            objectClass.name = "Ram";
+
+            Console.WriteLine(objectClass.id);
+            Console.WriteLine(objectClass.name);
+        }
+    }
+}
